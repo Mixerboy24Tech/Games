8cd79a1c9c496dec2b3d3afd05c2f9b9df997356
Atte Oksanen <37768995+Mixerboy24@users.noreply.github.com>
2020-02-03 10:40:36 +0200

    Update Obstacle.cs

--- Android/FlappyX/scripts/Obstacle.cs
+++ Android/FlappyX/scripts/Obstacle.cs
@@ -4,10 +4,12 @@ public class Obstacle : MonoBehaviour
 {
 	public Vector2 velocity = new Vector2(-4, 0);
 	public float range = 4;
+    public Rigidbody2D rigidbody2D;
 
-	void Start()
+    // Use this for initialization
+    void Start()
 	{
-		rigidbody2D.velocity = velocity;
+		GetComponent<Rigidbody2D>().velocity = velocity;
 		transform.position = new Vector3(transform.position.x, transform.position.y - range * Random.value, transform.position.z);
 	}
 }
